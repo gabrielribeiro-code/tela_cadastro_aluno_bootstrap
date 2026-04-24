c068aeab57610fbaa1ec59e93af3e84c0ce0234e
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-04-24 09:44:32 -0300

    feat: Fiz algumas validações dentro do javascript.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,42 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
+let form = document.getElementById('form-login');
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+form.addEventListener('submit', function(e){
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+    e.preventDefault();
+
+
+    let valido = true;
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let dataNascimento = document.getElementById('dataNascimento').value;
+    let endereco = document.getElementById('endereco').value;
+    let cidade = document.getElementById('cidade').value;
+    let estado = document.getElementById('estado').value;
+    let cep = document.getElementById('cep').value;
+    let curso = document.getElementById('curso').value;
+    let matricula = document.getElementById('matricula').value;
+    
+    let confirmarSenha = document.getElementById('confirmarSenha').value;
+    let cpf = document.getElementById('cpf').value;
+    let cpfCadastrados = ["125678900", "11111111111"];
+
+
+     if (nome.length < 10)  return alert("Nome tem que ser completo");
+     if(!email.includes("@") || !email.includes(".")) return alert("Email Inválido");
+     if(telefone.length < 9) return alert("Telefone Inválido");
+     if(cpf == "") return alert("CPF Obrigatorio");
+     if(cpfCadastrados.includes(cpf)) return alert("CPF já cadastrado");
+     if(endereco.length < 20) return alert ("Endereço Inválido");
+     if(senha.length < 4 ) return alert("SENHA MUITO CURTA!");
+     if(confirmarSenha != senha) return alert("SENHA ERRADA, NÃO É A MESMA SENHA!");
+
+     
+       
+
+    
+
+})
